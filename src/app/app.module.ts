3714cb35b67a7f402566327c1e686e9bffee332c
touqeermuhammad <touqeer.muhammad@live.com>
2020-01-23 14:39:50 +0000

    2 files modified, 6 added, 6 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,12 +9,15 @@ import { EcuTekFilterComponent } from './EcuTekComponents/EcuTekFilter/ecutek-fi
 import { EcuTekMenuComponent } from './EcuTekComponents/EcuTekMenu/ecutek-menu.component';
 
 import { EcuTekDropDownListComponent } from './EcuTekComponents/EcuTekDropDownList/ecutek-dropdownlist.component';
-
 import {ExportComponent} from './ExportComponent/export.component'
+
+import {CrisisListComponent} from './RountingComponents/CrisisListComponent/crisis-list.component';
+import {HeroListComponet} from './RountingComponents/HeroListComponet/hero-list.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, TestComponent, EcuTekGridComponent, EcuTekFilterComponent, EcuTekMenuComponent, EcuTekDropDownListComponent, ExportComponent ],
+  declarations: [ AppComponent, TestComponent, EcuTekGridComponent, EcuTekFilterComponent, EcuTekMenuComponent, EcuTekDropDownListComponent, ExportComponent, CrisisListComponent, HeroListComponet ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
